587e3a2f7d9ffec81aa1f75ffc9158edc60c5d5e
Ruel Coy <rcoy6832@floridapoly.edu>
2019-10-23 22:35:20 -0400

    Add project files.

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
+        public int ExperiencePoints { get; set; }
+        
+        //public Player(string name, string characterClass, int hitPoints, int gold, int level, int experiencePoints)
+        //{
+        //    Name = name;
+        //    CharacterClass = characterClass;
+        //    HitPoints = hitPoints;
+        //    Gold = gold;
+        //    Level = level;
+        //    ExperiencePoints = experiencePoints;
+        //}
+        
+    }
+}
